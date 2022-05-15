8a8b077f2c91716cfc7febbfb7523775bab709c8
Cheol-Cheol <dream174822@gmail.com>
2022-05-16 00:35:00 +0900

    ✏️ 완성

--- react/image-gallery/src/App.tsx
+++ react/image-gallery/src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useCallback } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ImageBox from "./components/ImageBox";
+import { useDropzone } from "react-dropzone";
 
 function App() {
+  const inpRef = useRef<HTMLInputElement>(null);
+  // <string[]> - string array를 뜻함
+  const [imageList, setImageList] = useState<string[]>([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    if (acceptedFiles.length) {
+      for (const file of acceptedFiles) {
+        const render = new FileReader();
+        render.readAsDataURL(file);
+        render.onloadend = (event) => {
+          // useState변경 함수 - prev: 이전 값을 가져올 수 있다.
+          setImageList((prev) => [...prev, event.target?.result as string]);
+        };
+      }
+    }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  console.log(imageList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className={"gallery-box " + (imageList.length > 0 && "row")}>
+        {imageList.length === 0 && (
+          <div className="text-center">
+            이미지가 없습니다.
+            <br />
+            이미지를 추가해 주세요.
+          </div>
+        )}
+
+        {imageList.map((el) => (
+          <ImageBox key={el} src={el} />
+        ))}
+        <div className="plus-box" {...getRootProps()}>
+          <input {...getInputProps} />+
+        </div>
+      </div>
     </div>
   );
 }
